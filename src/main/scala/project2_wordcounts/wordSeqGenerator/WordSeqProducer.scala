2f5c48e9f76d359a6985808d63dbd04bdba6aff9
yinchungui <1794113800@qq.com>
2020-06-07 21:02:07 +0800

    spark streaming实时流运算

--- src/main/scala/project2_wordcounts/wordSeqGenerator/WordSeqProducer.scala
+++ src/main/scala/project2_wordcounts/wordSeqGenerator/WordSeqProducer.scala
@@ -28,39 +28,35 @@ import org.apache.kafka.clients.producer.ProducerRecord
 
 
  */
-object WordSeqProducer extends App{
-    val events = 1000    //生成评论条数
-    val topic = "comments"
+object WordSeqProducer extends App {
+  val events = 10 //生成评论条数
+  val topic = "comments"   //主题名
     val brokers = "node1:9092,node2:9092,node3:9092"
-    val rnd = new Random()
-    val props = new Properties()
-    props.put("bootstrap.servers", brokers)
-    props.put("client.id", "wordFreqGenerator")
-    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
-    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+  val rnd = new Random()    //用于随机地生成文本。
 
-    val producer = new KafkaProducer[String, String](props)
-    val t = System.currentTimeMillis()
+  val props = new Properties()   //kafka producer参数
+  props.put("bootstrap.servers", brokers)
+  props.put("client.id", "wordFreqGenerator")
+  props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+  props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
 
-    // 读取汉字字典
-    val source = scala.io.Source.fromFile("data/hanzi.txt")
-    val lines = try source.mkString finally source.close()
-    for (nEvents <- Range(0, events)) {
-      // 生成模拟评论数据(user, comment)
-      val sb = new StringBuilder()
-      for (ind <- Range(0, rnd.nextInt(200))) {
-        sb += lines.charAt(rnd.nextInt(lines.length()))
-      }
-      val userName = "user_" + rnd.nextInt(100)
-      val data = new ProducerRecord[String, String](topic, userName, sb.toString())
-
-      //async
-      //producer.send(data, (m,e) => {})
-      //sync
-      producer.send(data)
+  val producer = new KafkaProducer[String, String](props)
+  val t = System.currentTimeMillis()   // 获取系统当前时间，用于性能检测
+  // 读取汉字字典
+  val source = scala.io.Source.fromFile("data/hanzi.txt")
+  val lines = try source.mkString finally source.close()
+  //控制生成 events 条消息
+  for (nEvents <- Range(0, events)) {
+    // 生成模拟评论数据(user, comment)
+    val sb = new StringBuilder()
+    //每条消息最多200个字符
+    for (ind <- Range(   0, rnd.nextInt(200)    )) {
+      sb += lines.charAt(     rnd.nextInt(lines.length())    )
     }
-
-    System.out.println("sent per second: " + events * 1000 / (System.currentTimeMillis() - t))
-    producer.close()
-
+    val userName = "user_" + rnd.nextInt(100)    // 主题   键        值
+    val data = new ProducerRecord[String, String](topic, userName, sb.toString())
+    producer.send(data)
+  }
+  System.out.println("每秒可以发送消息: " + events * 1000 / (System.currentTimeMillis() - t))
+  producer.close()
 }
